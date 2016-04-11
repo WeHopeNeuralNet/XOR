f46d9a9cb94c19054ca5af6315a95540e3826c1e
Ryuichiroh Iheuchi <an94_49@yahoo.co.jp>
2016-04-12 02:30:41 +0900

    hvbauwinor

--- xor/net.py
+++ xor/net.py
@@ -5,20 +5,21 @@ import math
 class NeuralNet:
     
     def __init__(self, shape):
-        self.init_weights(shape)
+        self.shape = shape
+        self.init_weights()
     
-    def init_weights(self, shape):
+    def init_weights(self):
         self.weights = {}
-        for layer, cur_layer in enumerate(shape[1:]):
-            prev_layer = shape[layer] + 1
+        for layer, cur_layer in enumerate(self.shape[1:]):
+            prev_layer = self.shape[layer] + 1
             self.weights[layer + 1] = np.random.rand(cur_layer, prev_layer)
     
     def train(self, x_datas, y_datas, max_iterations=1000, error_threshold=0.05):
         iteration_count = 0
         while iteration_count < max_iterations:
             for x_data, y_data in six.moves.zip(x_datas, y_datas):
-                output = self.forward(x_data)
-                self.back(x_data, y_data, output)
+                outputs = self.forward(x_data)
+                self.back(x_data, y_data, outputs)
             
             if self.check():
                 break
@@ -28,7 +29,7 @@ class NeuralNet:
     def forward(self, x_data):
         outputs = {}
         for layer, unit_weights in six.iteritems(self.weights):
-            output = np.array([], dtype=np.float32).reshape(-1, 1)
+            output = np.array([]).reshape(-1, 1)
             prev_output = outputs[layer - 1] if outputs else x_data
             prev_output = np.append(prev_output, 1)
             for weight in unit_weights:
@@ -38,12 +39,19 @@ class NeuralNet:
             outputs[layer] = output
         return outputs
     
+    def back(self, x_data, y_data, outputs):
+        outputs[len(self.shape) - 2] = np.append(outputs[len(self.shape) - 2], 1)
+        for i, last_hidden_output in enumerate(outputs[len(self.shape) - 2]):
+            for j, last_output in enumerate(outputs[len(self.shape) - 1]):
+                self.weights[len(self.shape) - 1][j][i] -= (y_data[j] - last_output) \
+                    * last_output * (1 - last_output) * last_hidden_output
+        
     def predict(self, layer):
         self.weights[layer].dot()
     
     def sigmoid(self, x):
         return 1/(1 + math.e ** (-x))
     
-        
+    
         
         
--- xor/xor.py
+++ xor/xor.py
@@ -2,5 +2,5 @@ import numpy as np
 from net import NeuralNet
 
 nn = NeuralNet([2, 3, 1])
-nn.train(np.array([[1, 0], [0, 0]]), [1, 0])
+nn.train(np.array([[1, 0], [0, 0]]), [[1], [0]])
 
